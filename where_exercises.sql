7872403c2aeb3fd724bc5dcbd858d9e5da6ac506
MarkGraebener <mark.w.graebener@gmail.com>
2022-03-21 17:56:38 -0500

    where

---
+++ where_exercises.sql
@@ -0,0 +1,26 @@
+USE employees_db;
+
+SELECT * FROM employees
+    WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT last_name FROM employees
+    WHERE last_name LIKE 'E%';
+
+SELECT last_name FROM employees
+    WHERE last_name LIKE '%q%';
+
+-- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+
+SELECT last_name FROM employees
+    WHERE last_name LIKE 'E%'
+    OR last_name LIKE '%e';
+
+-- Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E'
+SELECT last_name FROM employees
+WHERE last_name LIKE 'E%'
+   AND last_name LIKE '%e';
+
+-- Find all employees with a 'q' in their last name but not 'qu'
+SELECT last_name FROM employees
+    WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%'; -- or do i use !=????
