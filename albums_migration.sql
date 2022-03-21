0028bb4e8150fe1312e1585fe62173bdc61b68fa
MarkGraebener <mark.w.graebener@gmail.com>
2022-03-21 14:35:21 -0500

    tables

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,5 +2,11 @@ USE codeup_test_db;
 
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-
+    artist VARCHAR(50) DEFAULT 'none',
+    album_name VARCHAR(50) DEFAULT 'none',
+    release_date INT UNSIGNED,
+    sales FLOAT UNSIGNED,
+    genra VARCHAR(50) DEFAULT 'none',
+    PRIMARY KEY (id)
 );
+
